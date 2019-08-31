a7f898242898d5486bf6bbfe3b9e9f3dcda57afa
Jadan Ercoli <jtercoli@iu.edu>
2019-08-31 06:21:56 -0400

    Updates LICENSE, README.md, and main.py

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Jadan Ercoli.')
+print('My favorite game would have to be Jak and Daxter 2.')
+print('In all honesty, I have no real concerns about this class.')
+print('In general, I am most excited about learning a bit more coding whilst working on games that I can influence creatively.')
+print('My stackoverflow.com user number is: 11991306')
+print('The URL to my github.com profile is: https://github.com/jtercoli')
